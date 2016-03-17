8a41fce50a7643bccfcb4fb3a41e315faacd2647
Ali-Amir Aldan <aliamir@mit.edu>
2016-03-17 19:00:43 -0400

    Added a files lib.

---
+++ lib/files.lua
@@ -0,0 +1,31 @@
+-- @module lib.files
+
+local files = {}
+local bytes = require('lib.bytes')
+
+function files.readfile(filename, format)
+  --[[ Reads a file and returns its contents in a requested format.
+  --
+  -- filename: String, pathname of the file.
+  -- format: String, either "byte" or "string", representing the type of format.
+  --         Default is "string".
+  -- return:
+  -- - String, if format == "string"; contents of file interpreted as plaintext.
+  -- - Array of bytes, if format == "byte"; contents of file interpreted as bytes.
+  --]]
+  text = ""
+  file = io.open(filename, "r")
+  for line in file:lines() do
+    text = text .. "\n" .. line
+  end
+  file:close()
+
+  format = format or "string"
+  if format == "string" then
+    return text
+  else
+    return bytes.string2bytearray(text)
+  end
+end
+
+return files
