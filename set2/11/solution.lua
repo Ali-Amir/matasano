5badb82a98079daad1410787f8263d669b6eedfa
Ali-Amir Aldan <aliamir@mit.edu>
2016-03-21 12:36:19 -0400

    Added a solution to challenge 11, according to my understanding of the problem.

--- set2/11/solution.lua
+++ set2/11/solution.lua
@@ -30,15 +30,51 @@ function encryption_oracle(data_org)
   -- Make a decision on the mode of encryption.
   mode = math.random(2)
   if mode == 1 then
-    print("CBC")
+    print("Encryption oracle used CBC")
     IV = bytes.random_bytearray(16)
     return cbc.encrypt(data, key, aes, {IV = IV})
   elseif mode == 2 then
-    print("ECB")
+    print("Encryption oracle used ECB")
     return ecb.encrypt(data, key, aes)
   else
     assert(false, "Something is wrong!")
   end
 end
 
+function ecb_cbc_detection_oracle(enc_ocl)
+  --[[ ECB/CBC detection oracle.
+  --
+  -- Given a black box that encrypts with either ECB/CBC on every call,
+  -- this method determines what mode took place after every call.
+  --
+  -- enc_ocl: Function, the encryption oracle.
+  -- return:
+  -- - String, either "CBC" or "ECB" depending on what the oracle was using.
+  --]]
+  test_string = ""
+  for i = 1,30 do
+    test_string = test_string .. "YELLOW_SUBMARINE"
+  end
+  encrypted = bytes.bytearray2string(enc_ocl(bytes.string2bytearray(test_string)))
+  is_ecb_flag = false
+  counts = {}
+  for i = 1,#encrypted,16 do
+    substr = encrypted:sub(i, i+15)
+    if counts[substr] == nil then
+      counts[substr] = 0
+    end
+    counts[substr] = counts[substr] + 1
+    if counts[substr] > 15 then
+      is_ecb_flag = true
+      break
+    end
+  end
+  if is_ecb_flag then
+    return "ECB"
+  else
+    return "CBC"
+  end
+end
+
 print(bytes.bytearray2hex(encryption_oracle("HELLO")))
+print(ecb_cbc_detection_oracle(encryption_oracle))
