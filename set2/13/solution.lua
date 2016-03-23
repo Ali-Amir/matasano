483016dd5856d7ace4dced9cdcd5f6bff9f7c6d2
Ali-Amir Aldan <aliamir@mit.edu>
2016-03-22 22:33:33 -0400

    Added function to create user profile strings.

--- set2/13/solution.lua
+++ set2/13/solution.lua
@@ -22,3 +22,28 @@ local map = parse_cookie('foo=bar&baz=qux&zap=zazzle')
 assert(map.foo == 'bar')
 assert(map.baz == 'qux')
 assert(map['zap'] == 'zazzle')
+
+function profile_for(email)
+  --[[ Creates a profile object for a given email and returns its string
+  -- representation.
+  --
+  -- email: String, without & and = characters. They will be ignored.
+  -- return:
+  -- - String, cookie representing the user object.
+  --   {
+  --     email: email,
+  --     uid: 123,
+  --     role: 'user'
+  --   }
+  --]]
+  -- Filter & and = characters.
+  email = email:gsub('&', '')
+  email = email:gsub('=', '')
+  -- Create a random uid.
+  math.randomseed(os.time())
+  local uid = math.random(1000000000)
+  -- Create the mapping.
+  return 'email='..email..'&uid='..uid..'&role=user'
+end
+
+print(profile_for('foo@bar'))
